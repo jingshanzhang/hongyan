b92b0ef646a97608bd50b4fb0da357c0b6e84246
jingshanzhang <1127480010@qq.com>
2017-07-27 14:16:13 +0800

    aa
    ddddddd

---
+++ CollectStatisList.class.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @brief                市占(数据收集工作量)
+ * @package
+ * @subpackage
+ * @author               $Author:   zhangjingshan <zhangjingshan@haozu.com>$
+ * @file                 $HeadURL$
+ * @version              $Rev$
+ * @lastChangeBy         $LastChangedBy$
+ * @lastmodified         $LastChangedDate$
+ * @copyright            Copyright (c) 2017.07.11, www.haozu.com
+ */
+class Action_Competitor_CollectStatisList extends Action_Common
+{
+    public function call(){
+        //{{{登陆验证
+        $authInfo = $this->_user_info;
+        $userInfo = $authInfo['data']['output']['data'];
+        //}}}
+        $arrParams = array();
+        $arrParams['user'] = $userInfo;
+        //获取提交数据
+        $arrParams['arrConds'] =$this->_getParams();
+        $ret = $this->_getProxyPage("Service_Page_Competitor_CollectStatis",$arrParams);
+        $this->_assign($ret['data']);
+        $tdkInfo = array("title"=>'数据收集工作量');
+        $this->assign('tdkInfo', $tdkInfo);
+        $this->assign('total', $ret['data']['total']);
+        $this->display('/competitor/collectstatis.tpl.php');
+    }
+}
